8fc561731c89ce07f63943b381c4a68b3b1dfeb8
Gabriel Penteado <gabripenteado@gmail.com>
2023-04-27 21:11:45 -0300

    created summary routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -71,5 +71,91 @@ export async function appRoutes(app: FastifyInstance) {
       completeProcedures,
     }
   })
+
+  // complete / not complete a procedure
+  app.patch('/procedures/:id/toggle', async (req) => {
+    const toggleProcedureParams = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = toggleProcedureParams.parse(req.params);
+
+    const today = dayjs().startOf('day').toDate();
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      }
+    })
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        }
+      })
+    }
+
+    const dayProcedure = await prisma.dayProcedure.findUnique({
+      where: {
+        day_id_procedure_id: {
+          day_id: day.id,
+          procedure_id: id,
+        }
+      }
+    })
+
+    if (dayProcedure) {
+      await prisma.dayProcedure.delete({
+        where: {
+          id: dayProcedure.id,
+        }
+      })
+    } else {
+      await prisma.dayProcedure.create({
+        data: {
+          day_id: day.id,
+          procedure_id: id,
+        }
+      })
+    }
+
+
+
+  })
+
+  app.get('/summary', async () => {
+    const summary = await prisma.$queryRaw`
+      SELECT 
+        D.id, 
+        D.date,
+        (
+          SELECT 
+            cast(count(*) as float)
+          FROM
+            day_procedures DP
+          WHERE 
+            DP.day_id = D.id
+        ) as completed,
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM
+            procedure_week_days PWD
+          JOIN procedures P
+            ON P.id = PWD.procedure_id
+          WHERE
+            PWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+            AND P.created_at <= D.date
+        ) as amount
+      FROM 
+        days D
+
+    `
+
+    return summary;
+  })
+
+
 }
 
