85e41a5d1f9e676d07c0e9f2dca826f93d689c39
Gabriel Penteado <gabripenteado@gmail.com>
2023-05-06 22:50:56 -0300

    created checkboxes

--- web/src/components/NewProcedureForm.tsx
+++ web/src/components/NewProcedureForm.tsx
@@ -1,4 +1,15 @@
 import { Check } from "phosphor-react";
+import * as Checkbox from '@radix-ui/react-checkbox';
+
+const availableWeekDays = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
 
 export function NewProcedureForm() {
   return (
@@ -19,8 +30,32 @@ export function NewProcedureForm() {
         Which is the recurrence?
       </label>
 
+      <div className="flex flex-col gap-2 mt-3">
+        {availableWeekDays.map(weekDay => {
+          return (
+            <Checkbox.Root
+              key={weekDay}
+              className='flex items-center gap-3 group'
+            >
+              <div className='h-8 w-8 rounded-lg flex items-center justify-center
+               bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-teal-500
+                group-data-[state=checked]:border-teal-500'>
+                <Checkbox.CheckboxIndicator>
+                  <Check size={20} className='text-white' />
+                </Checkbox.CheckboxIndicator>
+              </div>
+
+              <span className=' text-white leading-tight'>
+                {weekDay}
+              </span>
+
+            </Checkbox.Root>
+          )
+        })}
+      </div>
+
       <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center
-       gap-3 font-semibold bg-green-600 hover:bg-green-500">
+       gap-3 font-semibold bg-teal-600 hover:bg-teal-500">
         <Check size={20} weight="bold" />
         Confirm
       </button>
--- web/src/components/ProcedureDay.tsx
+++ web/src/components/ProcedureDay.tsx
@@ -1,6 +1,9 @@
 import * as Popover from '@radix-ui/react-popover';
+import * as Checkbox from '@radix-ui/react-checkbox';
+
 import { ProgressBar } from './ProgressBar';
 import clsx from 'clsx';
+import { Check } from 'phosphor-react';
 
 interface ProcedureDayProps {
   completed: number;
@@ -29,6 +32,25 @@ export function ProcedureDay(props: ProcedureDayProps) {
           <span className='font-extrabold mt-1 leading-tight text-3xl'>04/28</span>
 
           <ProgressBar progress={completedPercentage} />
+
+          <div className='mt-6 flex flex-col gap-3'>
+            <Checkbox.Root
+              className='flex items-center gap-3 group'
+            >
+              <div className='h-8 w-8 rounded-lg flex items-center justify-center
+               bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-teal-500
+                group-data-[state=checked]:border-teal-500'>
+                <Checkbox.CheckboxIndicator>
+                  <Check size={20} className='text-white' />
+                </Checkbox.CheckboxIndicator>
+              </div>
+
+              <span className='font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400'>
+                test
+              </span>
+
+            </Checkbox.Root>
+          </div>
 
           <Popover.Arrow height={8} width={16} className='fill-zinc-900' />
         </Popover.Content>
