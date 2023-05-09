821ee7ff89a7e063ba31353983133ef2a37dffb4
Gabriel Penteado <gabripenteado@gmail.com>
2023-05-08 21:04:32 -0300

    get form data

--- web/src/components/NewProcedureForm.tsx
+++ web/src/components/NewProcedureForm.tsx
@@ -1,5 +1,6 @@
 import { Check } from "phosphor-react";
 import * as Checkbox from '@radix-ui/react-checkbox';
+import { FormEvent, useState } from "react";
 
 const availableWeekDays = [
   'Sunday',
@@ -12,8 +13,29 @@ const availableWeekDays = [
 ];
 
 export function NewProcedureForm() {
+  const [title, setTitle] = useState('');
+  const [weekDays, setWeekDays] = useState<number[]>([])
+
+  function createNewProcedure(event: FormEvent) {
+    event.preventDefault();
+    console.log(title);
+    console.log(weekDays);
+  }
+
+  function handleToggleWeekDay(weekDay: number) {
+    if (weekDays.includes(weekDay)) {
+      const weekDaysWithRemovedOne = weekDays.filter(day => day !== weekDay);
+
+      setWeekDays(weekDaysWithRemovedOne);
+    } else {
+      const weekDaysWithAddedOne = [...weekDays, weekDay];
+
+      setWeekDays(weekDaysWithAddedOne);
+    }
+  }
+
   return (
-    <form className="w-full flex flex-col mt-6">
+    <form onSubmit={createNewProcedure} className="w-full flex flex-col mt-6">
       <label htmlFor="title" className="font-semibold leading-tight">
         Which is your new procedure?
       </label>
@@ -24,6 +46,7 @@ export function NewProcedureForm() {
         placeholder="Type your new procedure here."
         className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
         autoFocus
+        onChange={event => setTitle(event.target.value)}
       />
 
       <label htmlFor="" className="font-semibold leading-tight mt-4">
@@ -31,11 +54,12 @@ export function NewProcedureForm() {
       </label>
 
       <div className="flex flex-col gap-2 mt-3">
-        {availableWeekDays.map(weekDay => {
+        {availableWeekDays.map((weekDay, index) => {
           return (
             <Checkbox.Root
               key={weekDay}
               className='flex items-center gap-3 group'
+              onCheckedChange={() => handleToggleWeekDay(index)}
             >
               <div className='h-8 w-8 rounded-lg flex items-center justify-center
                bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-teal-500
